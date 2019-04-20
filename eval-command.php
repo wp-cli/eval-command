ab74281a2997e6c3e34f2b048015064c4b214e79
jrfnl <jrfnl@users.noreply.github.com>
2019-04-20 18:25:44 +0200

    CS: rename the $autoload variable

--- eval-command.php
+++ eval-command.php
@@ -4,9 +4,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
-if ( file_exists( $autoload ) ) {
-	require_once $autoload;
+$wpcli_eval_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $wpcli_eval_autoloader ) ) {
+	require_once $wpcli_eval_autoloader;
 }
 
 WP_CLI::add_command( 'eval', 'Eval_Command' );
